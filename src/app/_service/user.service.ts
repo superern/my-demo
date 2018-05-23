6cb3cbdfaa32676d97b42dc9326b2b46453ceab2
ErnAndrewGregorio <erngreg@ph.ibm.com>
2018-05-23 21:38:20 +0800

    fix tslint suggestions. update api Root url

--- src/app/_service/user.service.ts
+++ src/app/_service/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {RequestOptions, Headers, Http, Response} from "@angular/http";
-import {Observable} from "rxjs/Observable";
-import {User} from "../_models/user";
+import {RequestOptions, Headers, Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {User} from '../_models/user';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
@@ -15,7 +15,7 @@ export class UserService {
     });
     static REQUEST_OPTIONS: RequestOptions = new RequestOptions({headers: UserService.headers});
 
-    private apiRootUrl  = 'http://localhost:8000/';
+    private apiRootUrl  = 'http://superern.bonntech.com.au/';
     private findAllUrl  = this.apiRootUrl + 'bonntech/api/users';
     private findByIdUrl = this.apiRootUrl + 'bonntech/api/users/';
     private createUrl   = this.apiRootUrl + 'bonntech/api/users/create';
@@ -23,39 +23,39 @@ export class UserService {
     private deleteUrl   = this.apiRootUrl + 'bonntech/api/users/delete';
     constructor(private http: Http) { }
 
-    findAll(): Observable<User[]>{
+    findAll(): Observable<User[]> {
         return this.http.get(this.findAllUrl)
             .map((response: Response) => response.json())
             .catch( this.defaultErrorHandler());
     }
 
-    findById(id: number): Observable<User>{
+    findById(id: number): Observable<User> {
         return this.http.get(this.findByIdUrl + '/' + id)
             .map((response: Response) => response.json())
             .catch(this.defaultErrorHandler());
     }
 
-    update(user: User): Observable<User>{
+    update(user: User): Observable<User> {
         return this.http.post(this.saveUrl, user, UserService.REQUEST_OPTIONS)
             .map((response: Response) => response.json())
             .catch(this.defaultErrorHandler());
     }
 
-    delete(data: Array<Object>): Observable<any>{
+    delete(data: Array<Object>): Observable<any> {
         return this.http.post(this.deleteUrl, JSON.stringify(data), UserService.REQUEST_OPTIONS)
             .map((response: Response) => response.json())
             .catch(this.defaultErrorHandler());
     }
 
-    add(user: User): Observable<User>{
+    add(user: User): Observable<User> {
         return this.http.post(this.createUrl, user, UserService.REQUEST_OPTIONS)
             .map((response: Response) => response.json())
-            .catch(this.defaultErrorHandler())
+            .catch(this.defaultErrorHandler());
     }
 
-    private defaultErrorHandler(){
-        return (error:any) => {
+    private defaultErrorHandler() {
+        return (error: any) => {
             return Observable.throw(error.json().error || 'server error');
-        }
+        };
     }
 }
