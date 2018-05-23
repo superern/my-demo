f4d2abbff995757cde68917a9968e38169ce57b2
ErnAndrewGregorio <erngreg@ph.ibm.com>
2018-05-23 21:31:36 +0800

    aggrid fix issue 0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpModule} from "@angular/http";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {AgGridModule} from "ag-grid-angular";
+import {HttpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AgGridModule} from 'ag-grid-angular';
 
-import {ToastrModule} from "ngx-toastr";
-import {ModalModule} from "ngx-bootstrap";
-import {FormsModule} from "@angular/forms";
+import {ToastrModule} from 'ngx-toastr';
+import {ModalModule} from 'ngx-bootstrap';
+import {FormsModule} from '@angular/forms';
 
-import {UserService} from "./_service/user.service";
+import {UserService} from './_service/user.service';
 import { AppComponent } from './app.component';
 import {AgGridComponent} from './ag-grid/ag-grid.component';
 import { CreateUserComponent } from './create-user/create-user.component';
